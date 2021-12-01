860ad64c79a03db8ffe124e3b4a097b7c457f210
ChumakAn <anastasiia.chumak.knm.2020@lpnu.ua>
2021-12-01 23:10:57 +0200

    Added new branch

--- src/components/Catalog/Loader.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Loader.module.scss";
-
-export const Loader = () => {
-    return (
-        <div className={styles.container}>
-            <div className={styles.loader}/>
-        </div>
-    )
-}
--- src/declaration.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.jpg';
-declare module '*.png';
-declare module '*.svg';
-declare module '*.scss' {
-    const content: {[className: string]: string};
-    export = content;
-}
